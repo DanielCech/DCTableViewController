f8a24ccd1906ab54670b92347a2a49bbc8476616
Dan <daniel.cech@gmail.com>
2016-10-14 01:32:30 +0200

    Add: menu controller

---
+++ DCTableViewController/Cells/LoadingCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingCell.swift
+//  DCTableViewController
+//
+//  Created by Dan on 13.10.16.
+//  Copyright © 2016 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoadingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DCTableViewController/Cells/MenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  MenuCell.swift
+//  DCTableViewController
+//
+//  Created by Dan on 13.10.16.
+//  Copyright © 2016 Dan. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
